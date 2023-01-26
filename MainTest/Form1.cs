b1c10c2eb90eacf1283eb789769a73d022796f5d
AjitGurav113 <ajit.gurav113@gmail.com>
2023-01-26 20:29:01 +0530

    btn code change

--- MainTest/Form1.cs
+++ MainTest/Form1.cs
@@ -19,7 +19,7 @@ namespace MainTest
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Welcome");
+            MessageBox.Show("Welcome Ajit");
         }
     }
 }
