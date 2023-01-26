6ebff460db93d80a2ecac7df4f6d6405df75396b
AjitGurav113 <ajit.gurav113@gmail.com>
2023-01-26 20:31:00 +0530

    code updated

--- MainTest/Form1.Designer.cs
+++ MainTest/Form1.Designer.cs
@@ -30,33 +30,46 @@ namespace MainTest
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(118, 73);
+            this.button1.Location = new System.Drawing.Point(157, 90);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(352, 116);
+            this.button1.Size = new System.Drawing.Size(469, 143);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(266, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(580, 261);
+            this.ClientSize = new System.Drawing.Size(773, 321);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- MainTest/Form1.cs
+++ MainTest/Form1.cs
@@ -19,7 +19,7 @@ namespace MainTest
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Welcome Ajit");
+            MessageBox.Show("Welcome " + textBox1.Text.Trim());
         }
     }
 }
